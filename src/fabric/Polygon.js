d12392aa4c2052267ab92b8d3e5025f15b6abe22
Agustin <perezp.agustin@gmail.com>
2020-04-02 21:20:14 -0300

    Applying update points for scale, rotate, and move polygon

--- src/fabric/Polygon.js
+++ src/fabric/Polygon.js
@@ -47,13 +47,6 @@ class Polygon {
   addPolygonListeners = () => {
     this.selectedPolygon.on('mousedblclick', e => {
       
-      console.log('current polygon', this.selectedPolygon)
-
-      console.log('cc0', this.selectedPolygon.getCoords())
-
-
-      console.log('current polygon points', this.selectedPolygon.points)
-
       if (this.selectedPolygon.get('selectable')) {
         this.selectedPolygon.set('selectable', false)
         this.canvasF.discardActiveObject()
@@ -81,9 +74,7 @@ class Polygon {
           .map(p => this.canvasF.remove(p))
           
         this.reDrawPolygon()
-
         this.selectedPolygon.set('selectable', true)
-        
         this.canvasF.add(this.selectedPolygon)
         this.canvasF.setActiveObject(this.selectedPolygon)
         this.addPolygonListeners()
@@ -92,6 +83,7 @@ class Polygon {
   }
 
   setCanvasListeners = () => {
+    //POINTS
     this.canvasF.on('object:moving', o => {
       if (!this.selectedPolygon) return
       const p = o.target
@@ -102,13 +94,108 @@ class Polygon {
         this.addPolygonListeners()
       }
     })
+    //POLYGONS
+    this.canvasF.on('object:moved', e => {
+      if (!this.selectedPolygon) return
+      const p = e.target
+      if (p.type !== 'POLYGON') return
+
+      const matrix = this.selectedPolygon.calcTransformMatrix()
+        const transformedPoints = this.selectedPolygon.points
+          .map(po => {
+            return new fabric.Point(
+              po.x - this.selectedPolygon.pathOffset.x,
+              po.y - this.selectedPolygon.pathOffset.y
+            )
+        }).map(pa => {
+          return fabric.util.transformPoint(pa, matrix)
+        })
+
+        this.canvasF.remove(this.selectedPolygon)
+        this.selectedPolygon = new fabric.Polygon(transformedPoints, {
+          id: this.getId(),
+          type: 'POLYGON',
+          fill: this.backgroundColor,
+          opacity: 0.5,
+          borderColor: 'grey',
+          borderOpacityWhenMoving: 1,
+          selectable: true,
+          objectCaching: false
+        })
+        
+        this.canvasF.add(this.selectedPolygon)
+        this.canvasF.setActiveObject(this.selectedPolygon)
+        this.addPolygonListeners()
+    })
+    this.canvasF.on('object:rotated', e => {
+      if (!this.selectedPolygon) return
+      const p = e.target
+      if (p.type !== 'POLYGON') return
+
+      const matrix = this.selectedPolygon.calcTransformMatrix()
+        const transformedPoints = this.selectedPolygon.points
+          .map(po => {
+            return new fabric.Point(
+              po.x - this.selectedPolygon.pathOffset.x,
+              po.y - this.selectedPolygon.pathOffset.y
+            )
+        }).map(pa => {
+          return fabric.util.transformPoint(pa, matrix)
+        })
+
+        this.canvasF.remove(this.selectedPolygon)
+        this.selectedPolygon = new fabric.Polygon(transformedPoints, {
+          id: this.getId(),
+          type: 'POLYGON',
+          fill: this.backgroundColor,
+          opacity: 0.5,
+          borderColor: 'grey',
+          borderOpacityWhenMoving: 1,
+          selectable: true,
+          objectCaching: false
+        })
+        
+        this.canvasF.add(this.selectedPolygon)
+        this.canvasF.setActiveObject(this.selectedPolygon)
+        this.addPolygonListeners()
+    })
+    this.canvasF.on('object:scaled', e => {
+      if (!this.selectedPolygon) return
+      const p = e.target
+      if (p.type !== 'POLYGON') return
+
+      const matrix = this.selectedPolygon.calcTransformMatrix()
+        const transformedPoints = this.selectedPolygon.points
+          .map(po => {
+            return new fabric.Point(
+              po.x - this.selectedPolygon.pathOffset.x,
+              po.y - this.selectedPolygon.pathOffset.y
+            )
+        }).map(pa => {
+          return fabric.util.transformPoint(pa, matrix)
+        })
+
+        this.canvasF.remove(this.selectedPolygon)
+        this.selectedPolygon = new fabric.Polygon(transformedPoints, {
+          id: this.getId(),
+          type: 'POLYGON',
+          fill: this.backgroundColor,
+          opacity: 0.5,
+          borderColor: 'grey',
+          borderOpacityWhenMoving: 1,
+          selectable: true,
+          objectCaching: false
+        })
+        
+        this.canvasF.add(this.selectedPolygon)
+        this.canvasF.setActiveObject(this.selectedPolygon)
+        this.addPolygonListeners()
+    })
   }
 
   reDrawPolygon = () => {
-
     this.canvasF.remove(this.selectedPolygon)
     const points = this.selectedPolygon.points
-
     this.selectedPolygon = new fabric.Polygon(points, {
       id: this.getId(),
       type: 'POLYGON',
@@ -119,7 +206,10 @@ class Polygon {
       selectable: false,
       objectCaching: false
     })
+  }
 
+  getCenterPoint = (x1, x2) => {
+    return (x1 + x2) / 2
   }
 
   
