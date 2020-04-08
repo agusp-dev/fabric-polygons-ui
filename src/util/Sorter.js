dca33bc8e43aaf4c5f4a6f38afff80ac45838635
Agustin <perezp.agustin@gmail.com>
2020-04-08 18:03:20 -0300

    merge

--- src/util/Sorter.js
+++ src/util/Sorter.js
@@ -4,76 +4,9 @@ export const Sorter = {
 }
 
 getArraySortered = array => {
-  let sorteredArray = []
-
-  let shorterDistance
   
-  array.forEach((e, i) => {
-
-    let currentItem = e
-
-    array.forEach(n => {
-
-      if (currentItem === n) break
-      let distance = getPointsDistance(currentItem, n)
-      if (!shorterDistance) {
-        shorterDistance = distance
-        
-      }
-
-
-
-
-
-
-    })
-
-
-
-
-  })
-
-  array.map((p, i) => {
-
-    let distance = getPointsDistance(p, array[i+1])
-    if (!shorterDistance) {
-      shorterDistance = distance
-      break
-    }
-
-
-
-
-
-
-
-
-
-
-    if (!minnor) {
-      minnor = distance
-      sorteredArray.push(p)
-      break
-    }
-
-    if (distance < minnor) {
-      minnor = distance
-      sorteredArray.push(p)
-    }
-
-
-
-
-
-
-  })
-
-
-
-
-
-  return sorteredArray
 }
+  
 
 getPointsDistance = (p1, p2) => {
   return Math.sqrt( 
